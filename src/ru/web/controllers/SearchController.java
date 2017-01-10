977c605bde5ddff7afcee3ba2b6bdae09fbd41c6
Sid <sid_andre@mail.ru>
2017-01-10 17:20:06 +0200

    work all searches

--- src/ru/web/controllers/SearchController.java
+++ src/ru/web/controllers/SearchController.java
@@ -22,9 +22,12 @@ import java.util.ResourceBundle;
 @SessionScoped
 public class SearchController implements Serializable {
 
+    private ArrayList<Integer> pageNumbers = new ArrayList<>();  //список из номеров страниц
     private SearchType searchType;  //хранит выбранный тип поиска
+    private String searchString;  //хранит строку поиска
     private static Map<String, SearchType> searchList = new HashMap<>();
     private ArrayList<Book> currentBookList;  //текущий список книг для отображения
+    private String currentSql;  //последний выполненный sql запрос без добавления limit
 
     public SearchController() {
         fillBooksAll();
@@ -102,6 +105,42 @@ public class SearchController implements Serializable {
                 "ORDER BY b.name");
     }
 
+    public void fillBooksBySearch() {
+
+        if(searchString.trim().length() == 0) {
+            fillBooksAll();
+            return;
+        }
+
+        StringBuilder sql = new StringBuilder("SELECT b.id, b.name, b.page_count, b.isbn, g.name as genre, a.fio as author, b.publish_year, " +
+                "p.name as publisher, b.image, b.descr " +
+                "FROM book b " +
+                "INNER JOIN genre g ON b.genre_id = g.id " +
+                "INNER JOIN author a ON b.author_id = a.id " +
+                "INNER JOIN publisher p ON b.publisher_id = p.id ");
+
+        if(searchType == SearchType.AUTHOR) {
+            sql.append("WHERE LOWER(a.fio) LIKE '%" + searchString.toLowerCase() + "%' ORDER BY b.name");
+        } else if (searchType == SearchType.TYTLE) {
+            sql.append("WHERE LOWER(b.name) LIKE '%" + searchString.toLowerCase() + "%' ORDER BY b.name");
+        }
+
+        fillBooksBySQL(sql.toString());
+    }
+
+    public void fillBooksByLetter() {
+        Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        String searchLetter = params.get("letter");
+        fillBooksBySQL("SELECT b.id, b.name, b.page_count, b.isbn, g.name as genre, a.fio as author, b.publish_year, " +
+                "p.name as publisher, b.image, b.descr " +
+                "FROM book b " +
+                "INNER JOIN genre g ON b.genre_id = g.id " +
+                "INNER JOIN author a ON b.author_id = a.id " +
+                "INNER JOIN publisher p ON b.publisher_id = p.id " +
+                "WHERE substr(b.name,1,1) = '" + searchLetter + "' " +
+                "ORDER BY b.name");
+    }
+
     public byte[] getImage(int id) {
         Connection conn = null;
         Statement stmt = null;
@@ -146,11 +185,23 @@ public class SearchController implements Serializable {
         return searchType;
     }
 
+    public void setSearchType(SearchType searchType) {
+        this.searchType = searchType;
+    }
+
     public Map<String, SearchType> getSearchList() {
         return searchList;
     }
 
     public ArrayList<Book> getCurrentBookList() {
         return currentBookList;
+    }
+
+    public String getSearchString() {
+        return searchString;
+    }
+
+    public void setSearchString(String searchString) {
+        this.searchString = searchString;
     }
 }
